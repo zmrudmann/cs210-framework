75eba03e7714319f4782f12f739e2deb69709d3d
Ron Reaser <ron@reaser.org>
2017-01-06 13:53:29 -0500

    Include Sandbox and unit testing examples

--- src/core/Sandbox.java
+++ src/core/Sandbox.java
@@ -1,16 +1,22 @@
 package core;
 
+import adt.Response;
 
 /** 
  * This class provides a sandbox for manual
  * testing of the rest of the project components.
  * 
  * It is not required for submission and can
- * be deleted if desired.
+ * be deleted if desired. You should use unit
+ * tests for testing when possible.
  */
 @Deprecated
 public class Sandbox {
 	public static void main(String[] args) {
+		Server SRV = new Server();
 		
+		Response response = SRV.interpret("ECHO \"Hello, world!\"").get(0);
+		
+		System.out.println(response.get("message"));
 	}
 }
