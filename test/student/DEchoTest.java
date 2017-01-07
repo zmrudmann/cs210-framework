ac5736cd309764936f760ebd60c573b64e3e8fb9
Ron Reaser <ron@reaser.org>
2017-01-07 13:18:56 -0500

    Added test for response count

--- test/student/DEchoTest.java
+++ test/student/DEchoTest.java
@@ -2,6 +2,8 @@ package student;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -18,23 +20,34 @@ import core.Server;
 @Deprecated
 public class DEchoTest {
 	private static Server SERVER;
+	private static List<Response> responses;
+	
 	private static String query;
-	private static Response response;
+	private static int count = 1;
 	
 	@BeforeClass
 	public static void initialize() {
 		SERVER = new Server();
 		
 		query = "ECHO \"Hello, world!\"";
-		response = SERVER.interpret(query).get(0);
+		responses = SERVER.interpret(query);
 	};
+	
+	@Test
+	public void testDriverQueryResponses() {
+		assertEquals(
+			"Query <" + query + "> must return " + count + "responses,",
+			count,
+			responses.size()
+		);
+	}
 	
 	@Test
 	public void testDriverEchoSuccess() {
 		assertEquals(
 			"Query <" + query + "> must succeed,",
 			true,
-			response.get("success")
+			responses.get(0).get("success")
 		);
 	}
 	
@@ -43,7 +56,7 @@ public class DEchoTest {
 		assertEquals(
 			"Query <" + query + "> must return correct message,",
 			"Hello, world!",
-			response.get("message")
+			responses.get(0).get("message")
 		);
 	}
 	
@@ -52,7 +65,7 @@ public class DEchoTest {
 		assertEquals(
 			"Query <" + query + "> must not return table,",
 			null,
-			response.get("table")
+			responses.get(0).get("table")
 		);
 	}
 }
